d85b9dd1eb820b2b123783fb249fc91137d9bc56
John O'Sullivan <josullivan1@luc.edu>
2018-06-11 19:55:53 -0500

    Initial Commit

---
+++ Crypto Wallet/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Crypto Wallet
+//
+//  Created by John O'Sullivan on 6/11/18.
+//  Copyright © 2018 John O'Sullivan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
