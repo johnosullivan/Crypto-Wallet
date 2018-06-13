92d85326cd6668d1e4b917354ad106530c615ee0
John O'Sullivan <josullivan1@luc.edu>
2018-06-13 17:44:52 -0500

    Fixed Helper

--- Crypto Wallet/Helpers/BuildHelper.swift
+++ Crypto Wallet/Helpers/BuildHelper.swift
@@ -6,10 +6,9 @@
 //  Copyright © 2018 John O'Sullivan. All rights reserved.
 //
 
-import Foundation
 import UIKit
 
-extension Build {
+extension Bundle {
     
     var versionAndBuild: String {
         let version = infoDictionary!["CFBundleShortVersionString"] as! String
