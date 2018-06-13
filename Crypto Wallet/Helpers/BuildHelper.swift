c34d026d1acbc0952482670c3151070cb097d076
John O'Sullivan <josullivan1@luc.edu>
2018-06-12 20:16:33 -0500

    Added Build helper

---
+++ Crypto Wallet/Helpers/BuildHelper.swift
@@ -0,0 +1,25 @@
+//
+//  BuildHelper.swift
+//  Crypto Wallet
+//
+//  Created by John O'Sullivan on 6/12/18.
+//  Copyright © 2018 John O'Sullivan. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension Build {
+    
+    var versionAndBuild: String {
+        let version = infoDictionary!["CFBundleShortVersionString"] as! String
+        let build = infoDictionary!["CFBundleVersion"] as! String
+        return "\(version) build \(build)"
+    }
+    
+    
+    var displayName: String {
+        return object(forInfoDictionaryKey: "CFBundleDisplayName") as! String
+    }
+    
+}
