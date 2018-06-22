77f78edb5df13f07e90197b24f52849cc090a641
John O'Sullivan <josullivan1@luc.edu>
2018-06-21 23:38:14 -0500

    Clean up viewcontroller

--- Crypto Wallet/AppMain/ViewController.swift
+++ Crypto Wallet/AppMain/ViewController.swift
@@ -20,135 +20,51 @@ extension Notification.Name {
     static let receive = Notification.Name("receive")
 }
 
-class ViewController: UIViewController, SyncCoordinatorDelegate {
+class ViewController: UIViewController {
     
     @IBOutlet weak var walletHeaderView: UIView!
     @IBOutlet weak var walletView: WalletView!
-    
     @IBOutlet weak var addCardViewButton: UIButton!
     
     let appDelegate = UIApplication.shared.delegate as! AppDelegate
-    
-    let keystore = KeystoreService()
-    
-    var tempcard: ColoredCardView!
-    
     var wallets = [ColoredCardView]()
     
     func sendPresention(notification:Notification) -> Void {
         print("sendPresention")
-        
     }
     
     func receivePresention(notification:Notification) -> Void {
         print("receivePresention")
-        
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         walletView.walletHeader = walletHeaderView
-        
         walletView.contentInset = UIEdgeInsets(top: 20, left: 0, bottom: 0, right: 0)
-        
-        //print(keystore.getAccountCount())
         
         let nc = NotificationCenter.default
         nc.addObserver(forName:.send, object:nil, queue:nil, using:sendPresention)
         nc.addObserver(forName:.receive, object:nil, queue:nil, using:receivePresention)
         
-        print((keystore.getAccountCount() - 1))
-        
-        for i in 0 ... (keystore.getAccountCount() - 1) {
+        for i in 0 ... (appDelegate.keyStore.getAccountCount() - 1) {
             let wallet = ColoredCardView.nibForClass()
             wallet.update(index: i)
             wallets.append(wallet)
         }
         
-        
         walletView.reload(cardViews: wallets)
-        
         walletView.didUpdatePresentedCardViewBlock = { [weak self] (_) in
             self?.showAddCardViewButtonIfNeeded()
             self?.addCardViewButton.addTransitionFade()
         }
-        
-        /*let acc = Account()
-        acc.address = ""
-        acc.name = ""
-        
-        let realm = try! Realm()
-
-        try! realm.write {
-            realm.add(acc)
-        }
-        
-        let accs = realm.objects(Account.self).filter("name == ''")
-        print(accs)*/
-
-        
     }
     
     func showAddCardViewButtonIfNeeded() {
         addCardViewButton.alpha = walletView.presentedCardView == nil || walletView.insertedCardViews.count <= 1 ? 1.0 : 0.0
     }
     
-     func syncDidChangeProgress(current: Int64, max: Int64) {
-        
-    }
-    func syncDidFinished() {
-        
-    }
-    func syncDidUpdateBalance(_ balanceHex: String, timestamp: Int64) {
-        
-    }
-    func syncDidUpdateGasLimit(_ gasLimit: Int64) {
-        
-    }
-    func syncDidReceiveTransactions(_ transactions: [GethTransaction], timestamp: Int64) {
-        print("Receive Transactions: ", transactions)
-    }
-    
     @IBAction func addCardViewAction(_ sender: Any) {
-        
-        for i in 0 ... (keystore.getAccountCount() - 1) {
-            wallets[i].update(index: i)
-        }
-        
-        //walletView.insert(cardView: ColoredCardView.nibForClass(), animated: true, presented: true)
-        /*print("Create Wallet")
-        let kstore = KeystoreService()
-        do {
-            try kstore.createAccount(passphrase: "mogilska")
-        } catch {
-            
-        }*/
-        
-        
-        /*
-        let chain = Chain.ropsten
-        
-        //let core = Ethereum.core
-        let keystore = KeystoreService()
-        let core = Ethereum.core
-        core.chain = chain
-        let syncCoordinator = StandardSyncCoordinator()
-        core.syncCoordinator = syncCoordinator
-        
-        do  {
-            print(try keystore.getAccount(at: 0).getAddress().getHex())
-
-            try syncCoordinator.startSync(chain: chain, delegate: self)
-            try core.client = syncCoordinator.getClient()
-        } catch {
-            
-        }*/
-        
-        let amount = Ether.init("1.0")
-        print(amount.raw.toWei())
-        //20000000000000000.0
-        //1000000000000000000
         
         /*
         let trans_service = TransactionService.init(core: core, keystore: keystore, transferType: TransferType.default)
@@ -203,17 +119,12 @@ class ViewController: UIViewController, SyncCoordinatorDelegate {
         */
         
     }
-    
-    
     override var prefersStatusBarHidden: Bool {
         return false
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-
     }
-
-
 }
 
